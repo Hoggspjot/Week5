72efe9e1ad07756c7b577b3554502354692c1d60
hoggspjot <Andreyid67@gmail.com>
2025-08-17 12:45:17 +0300

    шаблон Decorator

--- src/main/java/Foot.java
+++
@@ -1,8 +0,0 @@
-public class Foot implements TripStrategy{
-    private double kmPerHour = 6.0;
-
-    @Override
-    public void travelTime(double distance) {
-        System.out.printf("Путешествие на " + distance + " километров пешком займет %.2f часов\n",distance / kmPerHour);
-    }
-}
--- src/main/java/TripStrategy.java
+++
@@ -1,5 +0,0 @@
-interface TripStrategy {
-
-    void travelTime(double distance);
-
-}
